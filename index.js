a876f4e7dc018fab9f0a1433127b32fd40477182
Ali-Ashraf-Abir <fazilabir88@gmail.com>
2023-06-07 02:45:02 +0600

    added finding user role from db

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.ravtcpm.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ravtcpm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +31,36 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db("MelodicAdventures");
+    const userCollection = database.collection("users");
+    app.post("/users",async (req,res)=>{
+
+        const body=req.body;
+        console.log(body)
+        const result = await userCollection.insertOne(body)
+        res.send()
+  
+      })
+
+    app.get ('/allusers',async (req,res)=>{
+
+        const cursor = userCollection.find().toArray();
+        const result = await cursor;
+        res.send(result)
+
+    })
+
+    app.get ('/currentuser/:email',async(req,res)=>{
+
+        const email=req.params.email;
+        console.log(email)
+        const result = await userCollection.find({email : email}).toArray()
+        res.send(result)
+
+
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
